82cc87cb65ba916f700ace08490810e405701605
Diego Hernandez <diego.hdz.cadena@gmail.com>
2025-06-02 16:12:03 -0600

    Refactor lexer and parser code for consistency and readability

--- CompilerProgram.py
+++ CompilerProgram.py
@@ -5,38 +5,37 @@ from sly import Lexer, Parser
 
 class BasicLexer(Lexer):
     tokens = {NAME, NUMBER, STRING, IF, THEN, ELSE, ENDIF}
-    ignore = '\t '
-    literals = {'=', '+', '-', '/',
-                '*', '(', ')', ',', ';', '<', '>', '!'}
+    ignore = "\t "
+    literals = {"=", "+", "-", "/", "*", "(", ")", ",", ";", "<", ">", "!"}
 
     # Define tokens as regular expressions
 
     # Keywords
-    IF = r'if'
-    THEN = r'then'
-    ELSE = r'else'
-    ENDIF = r'endif'
+    IF = r"if"
+    THEN = r"then"
+    ELSE = r"else"
+    ENDIF = r"endif"
 
     # store as raw strings
-    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    STRING = r'\".*?\"'
+    NAME = r"[a-zA-Z_][a-zA-Z0-9_]*"
+    STRING = r"\".*?\""
 
     # Number token
-    @_(r'\d+')
+    @_(r"\d+")
     def NUMBER(self, t):
         # convert it into a python integer
         t.value = int(t.value)
         return t
 
     # Comment token
-    @_(r'//.*')
+    @_(r"//.*")
     def COMMENT(self, t):
         pass
 
     # Newline token(used only for showing errors in new line)
-    @_(r'\n+')
+    @_(r"\n+")
     def newline(self, t):
-        self.lineno = t.value.count('\n')
+        self.lineno = t.value.count("\n")
 
 
 class BasicParser(Parser):
@@ -44,40 +43,40 @@ class BasicParser(Parser):
     tokens = BasicLexer.tokens
 
     precedence = (
-        ('left', 'EQUALS_TO'),
-        ('left', '<', '>'),
-        ('left', 'IF', 'THEN', 'ELSE', 'ENDIF'),
-        ('left', '+', '-'),
-        ('left', '*', '/'),
-        ('right', 'UMINUS'),
+        # ("left", "EQUALS_TO"),
+        ("left", "<", ">"),
+        ("left", "IF", "THEN", "ELSE", "ENDIF"),
+        ("left", "+", "-"),
+        ("left", "*", "/"),
+        ("right", "UMINUS"),
     )
 
     def __init__(self):
         self.env = {}
 
-    @_('')
+    @_("")
     def statement(self, p):
         pass
 
-    @_('if_stmt')
+    @_("if_stmt")
     def statement(self, p):
         return p.if_stmt
 
-    @_('var_assign')
+    @_("var_assign")
     def statement(self, p):
         return p.var_assign
 
     @_('NAME "=" expr')
     def var_assign(self, p):
-        return ('var_assign', p.NAME, p.expr)
+        return ("var_assign", p.NAME, p.expr)
 
     @_('NAME "=" STRING')
     def var_assign(self, p):
-        return ('var_assign', p.NAME, p.STRING)
+        return ("var_assign", p.NAME, p.STRING)
 
-    @_('expr')
+    @_("expr")
     def statement(self, p):
-        return (p.expr)
+        return p.expr
 
     @_('"(" expr ")"')
     def expr(self, p):
@@ -85,47 +84,47 @@ class BasicParser(Parser):
 
     @_('expr "+" expr')
     def expr(self, p):
-        return ('add', p.expr0, p.expr1)
+        return ("add", p.expr0, p.expr1)
 
     @_('expr "-" expr')
     def expr(self, p):
-        return ('sub', p.expr0, p.expr1)
+        return ("sub", p.expr0, p.expr1)
 
     @_('expr "*" expr')
     def expr(self, p):
-        return ('mul', p.expr0, p.expr1)
+        return ("mul", p.expr0, p.expr1)
 
     @_('expr "/" expr')
     def expr(self, p):
-        return ('div', p.expr0, p.expr1)
+        return ("div", p.expr0, p.expr1)
 
     @_('expr "<" expr')
     def expr(self, p):
-        return ('lt', p.expr0, p.expr1)
+        return ("lt", p.expr0, p.expr1)
 
     @_('expr ">" expr')
     def expr(self, p):
-        return ('gt', p.expr0, p.expr1)
+        return ("gt", p.expr0, p.expr1)
 
-    @_('IF expr THEN statement ENDIF')
+    @_("IF expr THEN statement ENDIF")
     def if_stmt(self, p):
-        return ('if', p.expr, p.statement, None)
+        return ("if", p.expr, p.statement, None)
 
-    @_('IF expr THEN statement ELSE statement ENDIF')
+    @_("IF expr THEN statement ELSE statement ENDIF")
     def if_stmt(self, p):
-        return ('if', p.expr, p.statement0, p.statement1)
+        return ("if", p.expr, p.statement0, p.statement1)
 
     @_('"-" expr %prec UMINUS')
     def expr(self, p):
         return p.expr
 
-    @_('NAME')
+    @_("NAME")
     def expr(self, p):
-        return ('var', p.NAME)
+        return ("var", p.NAME)
 
-    @_('NUMBER')
+    @_("NUMBER")
     def expr(self, p):
-        return ('num', p.NUMBER)
+        return ("num", p.NUMBER)
 
 
 class BasicExecute:
@@ -148,34 +147,34 @@ class BasicExecute:
         if node is None:
             return None
 
-        if node[0] == 'program':
+        if node[0] == "program":
             if node[1] == None:
                 self.walkTree(node[2])
             else:
                 self.walkTree(node[1])
                 self.walkTree(node[2])
 
-        if node[0] == 'num':
+        if node[0] == "num":
             return node[1]
 
-        if node[0] == 'str':
+        if node[0] == "str":
             return node[1]
 
-        if node[0] == 'add':
+        if node[0] == "add":
             return self.walkTree(node[1]) + self.walkTree(node[2])
-        elif node[0] == 'sub':
+        elif node[0] == "sub":
             return self.walkTree(node[1]) - self.walkTree(node[2])
-        elif node[0] == 'mul':
+        elif node[0] == "mul":
             return self.walkTree(node[1]) * self.walkTree(node[2])
-        elif node[0] == 'div':
+        elif node[0] == "div":
             return self.walkTree(node[1]) / self.walkTree(node[2])
 
-        if node[0] == 'lt':
+        if node[0] == "lt":
             return self.walkTree(node[1]) < self.walkTree(node[2])
-        elif node[0] == 'gt':
+        elif node[0] == "gt":
             return self.walkTree(node[1]) > self.walkTree(node[2])
 
-        if node[0] == 'if':
+        if node[0] == "if":
             condition = self.walkTree(node[1])
             print("Condition evaluated to:", condition)
             print("node", node)
@@ -184,36 +183,52 @@ class BasicExecute:
             elif node[3] is not None:
                 return self.walkTree(node[3])  # else branch
 
-        if node[0] == 'var_assign':
+        if node[0] == "var_assign":
             self.env[node[1]] = self.walkTree(node[2])
             return node[1]
 
-        if node[0] == 'var':
+        if node[0] == "var":
             try:
                 return self.env[node[1]]
             except LookupError:
-                print("Undefined variable '"+node[1]+"' found!")
+                print("Undefined variable '" + node[1] + "' found!")
                 return 0
 
 
 # Displaying the Program output
-if __name__ == '__main__':
+if __name__ == "__main__":
     lexer = BasicLexer()
     parser = BasicParser()
-    print('TC3002B Programming Language Program 1.0 (tags/v3.10.11:7d4cc5a, Jun 1 2025, 00:38:17')
-    print('TC3002B Programming Language Program')
-    print('A00833623 - Ivan Romero')
-    print('A00834015 - Diego Hernandez')
+    print(
+        "TC3002B Programming Language Program 1.0 (tags/v3.10.11:7d4cc5a, Jun 1 2025, 00:38:17"
+    )
+    print('Type "help", "credits" or "exit" to exit the program.')
+    # print("A00833623 - Ivan Romero")
+    # print("A00834015 - Diego Hernandez")
     env = {}
 
     while True:
 
         try:
-            text = input('Make input to Program here> ')
+            text = input(">> ")
 
         except EOFError:
             break
 
         if text:
+            if text.strip().lower() in ("exit", "quit"):
+                break
+            elif text.strip().lower() == "help":
+                print("This is a simple interpreter for a basic programming language.")
+                print(
+                    "You can use variables, arithmetic operations, and if statements."
+                )
+                continue
+            elif text.strip().lower() == "credits":
+                print(
+                    "Developed by Ivan Romero (A00833623) and Diego Hernandez (A00834015)."
+                )
+                continue
+
             tree = parser.parse(lexer.tokenize(text))
             BasicExecute(tree, env)
